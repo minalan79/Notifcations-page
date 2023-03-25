298e36f6d69db97984dde04903515fbea58d2c71
Minalan <minalan79@gmail.com>
2023-03-25 11:23:09 +0530

    fixed notifications

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const markread = document.getElementsByClassName('mark-read');
+const unreads = document.getElementsByClassName('notification-unread');
+
+document.getElementsByClassName('notification-number')[0].innerHTML = `${unreads.length}`;
 
 markread[0].addEventListener('click', () => {
-  const unreads = document.getElementsByClassName('notification-unread');
   for (let i = 0; i < unreads.length; i++) {
     const unread = unreads[i];
     unread.style.backgroundColor = 'white';
-    // console.log(unread.children);
+    console.log(unread.children);
     // unread.children[1].children[1].style.display = 'none';
     unread.children[1].children[1].classList.add('read');
   }
   // unreads.forEach( unread => {
   //   console.log(unread);
   // });
+  document.getElementsByClassName('notification-number')[0].innerHTML = "0";
 })
